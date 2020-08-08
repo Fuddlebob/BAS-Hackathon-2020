da4aa4cf2c32a68ebf46651d98e95193112937cb
thomascytang <thomas@Thomas-PC.localdomain>
2020-08-09 05:07:24 +1000

    Video generation

--- orbot.py
+++ orbot.py
@@ -1,39 +1,71 @@
 import random
 import numpy as np
+import cv2
+import math
 
 #Bot to simulate gravitational bodies
 #made for BAS Hackathon 2020
 #Author: Fuddlebob
 
 G = 6.67e-8
+canvas_size = (480, 480, 3)
 
 class Body(object):
-	def __init__(self, mass, position, velocity, id):
+	def __init__(self, mass, position, velocity, id, colour):
 		self.mass = mass
 		self.position = position
 		self.velocity = velocity
 		self.id = id
+		self.colour = colour
 		
 		
 		
 		
 def main():
 	bodies = []
-	numBodies = random.randint(2, 2)
+	numBodies = random.randint(2, 7)
 	print(numBodies)
 	for i in (range(numBodies)):
-		mass = random.uniform(1, 5)
-		position = np.array([(random.random() - 0.5), (random.random() - 0.5)])
-		velocity = np.array([(random.random() - 0.5), (random.random() - 0.5)]) / 100
-		bodies.append(Body(mass, position, velocity, i))
-		print("ID: " + str(i) + " Mass: " + str(mass) + " Position: " + str(position) + " Velocity: " + str(velocity))
+		mass = random.uniform(20, 200)
+		position = np.array([(random.random() + 0.5), (random.random() + 0.5)])
+		velocity = np.array([(random.random() - 0.5), (random.random() - 0.5)])
+		colour = (random.randint(50, 255),random.randint(50, 255),random.randint(50, 255))
+		bodies.append(Body(mass, position, velocity, i, colour))
 	
-	step(bodies)
+	canvas = np.zeros(canvas_size, np.uint8)
+	video_name = "vid.avi"
+	vid = cv2.VideoWriter(video_name, 0, 20, (480, 480))
+	clearcount = 0
+	for i in range(600):
+		bodies = step(bodies)
+		im = render(bodies, canvas.copy())
+		vid.write(im)
+		if(not im.any()):
+			clearcount += 1
+			if(clearcount >= 10):
+				break
+		else:
+			clearcount = 0
+	vid.release()
+		
 	
-	print("After one step:")
-	for b in bodies:
-		print("ID: " + str(b.id) + " Mass: " + str(b.mass) + " Position: " + str(b.position) + " Velocity: " + str(b.velocity))
 
+
+def render(bodies, canvas):
+	if(canvas is None):
+		canvas = np.zeros(canvas_size, np.uint8)
+	size = max(canvas_size[0], canvas_size[1])
+
+	for b in bodies:
+		x = int(((b.position[0] * size))/2)
+		y = int(((b.position[1] * size))/2)
+		canvas = cv2.circle(canvas, (x,y), mass2size(b.mass), b.colour, thickness=-1)
+	return canvas
+	
+
+def mass2size(m):
+	return int(math.sqrt((m * 10)/math.pi))
+	
 #calculates distance between two points
 def dist(p1, p2):
 	return np.linalg.norm(p1 - p2)
@@ -61,7 +93,7 @@ def step(bodies):
 	#update each velocity and apply the new velocity to the position
 	for b in bodies:
 		b.velocity = b.velocity + deltavs[b.id]
-		b.position = b.position + b.velocity
+		b.position = b.position + (b.velocity / 50)
 		
 	return bodies
 
