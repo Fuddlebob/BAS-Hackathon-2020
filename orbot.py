51d0fc87bd4414d3869113d34467db12f04ab653
thomascytang <thomas@Thomas-PC.localdomain>
2020-08-09 16:20:30 +1000

    Planets now leave slowly fading trails

--- orbot.py
+++ orbot.py
@@ -2,13 +2,14 @@ import random
 import numpy as np
 import cv2
 import math
+import copy
 
 #Bot to simulate gravitational bodies
 #made for BAS Hackathon 2020
 #Author: Fuddlebob
 
 G = 6.67e-8
-canvas_size = (480, 480, 3)
+canvas_size = (480, 720, 3)
 
 class Body(object):
 	def __init__(self, mass, position, velocity, id, colour):
@@ -27,34 +28,35 @@ def main():
 	print(numBodies)
 	for i in (range(numBodies)):
 		mass = random.uniform(20, 200)
-		position = np.array([(random.random() + 0.5), (random.random() + 0.5)])
-		velocity = np.array([(random.random() - 0.5), (random.random() - 0.5)])
+		position = np.array([(random.random() * 1.5) + 0.25, (random.random() * 1.5) + 0.25])
+		velocity = np.array([(random.random() - 0.5) * 1.5, (random.random() - 0.5) * 1.5])
 		colour = (random.randint(50, 255),random.randint(50, 255),random.randint(50, 255))
 		bodies.append(Body(mass, position, velocity, i, colour))
 	
 	canvas = np.zeros(canvas_size, np.uint8)
 	video_name = "vid.avi"
-	vid = cv2.VideoWriter(video_name, 0, 20, (480, 480))
+	vid = cv2.VideoWriter(video_name, 0, 20, (canvas_size[1], canvas_size[0]))
 	clearcount = 0
 	for i in range(600):
+		#create a deep copy since we're modifying the objects inside the list, rather than just the list
+		oldBodies = copy.deepcopy(bodies)
 		bodies = step(bodies)
+		canvas = mark_path(oldBodies, bodies, canvas)
 		im = render(bodies, canvas.copy())
 		vid.write(im)
-		if(not im.any()):
+		if((im < 15).all()):
 			clearcount += 1
-			if(clearcount >= 10):
+			if(clearcount >= 30):
 				break
 		else:
 			clearcount = 0
 	vid.release()
 		
-	
-
 
 def render(bodies, canvas):
 	if(canvas is None):
 		canvas = np.zeros(canvas_size, np.uint8)
-	size = max(canvas_size[0], canvas_size[1])
+	size = min(canvas_size[0], canvas_size[1])
 
 	for b in bodies:
 		x = int(((b.position[0] * size))/2)
@@ -62,7 +64,17 @@ def render(bodies, canvas):
 		canvas = cv2.circle(canvas, (x,y), mass2size(b.mass), b.colour, thickness=-1)
 	return canvas
 	
-
+def mark_path(oldBodies, newBodies, canvas):
+	canvas = (canvas * 0.99).astype(np.uint8)
+	size = min(canvas_size[0], canvas_size[1])
+	for i in range(len(oldBodies)):
+		oldx = int(((oldBodies[i].position[0] * size))/2)
+		oldy = int(((oldBodies[i].position[1] * size))/2)
+		newx = int(((newBodies[i].position[0] * size))/2)
+		newy = int(((newBodies[i].position[1] * size))/2)
+		canvas = cv2.line(canvas, (oldx, oldy), (newx, newy), oldBodies[i].colour, thickness = 2)
+	
+	return canvas
 def mass2size(m):
 	return int(math.sqrt((m * 10)/math.pi))
 	
@@ -95,7 +107,7 @@ def step(bodies):
 		b.velocity = b.velocity + deltavs[b.id]
 		b.position = b.position + (b.velocity / 50)
 		
-	return bodies
+	return bodies.copy()
 
 
 
